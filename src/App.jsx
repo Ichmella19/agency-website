8223a80df48da16bc9c8561caeff9fb1d9a24501
Elcy <ichmellatognide@gmail.com>
2024-09-25 18:12:43 +0200

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+const App = () => {
+    return (
+        <div className="">
+            <h1 className="font-bold text-xl leading-3 bg-blue-600">Hello,  j{"'"}apprends React</h1>
+        </div>
+      );
+}
+ 
+export default App;
---
+++ src/composants/HelloSection.jsx
@@ -0,0 +1,13 @@
+import { FaGithub } from "react-icons/fa";
+const HelloSection = () => {
+    return ( 
+        <div>
+            <div className="bg-red-500 flex justify-center">Elcy</div>
+            <a href="" className="">
+            <FaGithub className="text-yellow-400 text-2xl "/>
+            </a>
+        </div>
+     );
+}
+ 
+export default HelloSection;
